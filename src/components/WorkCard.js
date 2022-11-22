d4ae1bcb0e51181757f571505822123de312af85
Benjamin Salmon <103594910+bamaben10@users.noreply.github.com>
2022-11-22 17:30:41 -0600

    Add files via upload
    Initial commit, creating my host website for portfolio

---
+++ src/components/WorkCard.js
@@ -0,0 +1,31 @@
+import "./WorkCardStyles.css";
+
+import React from "react";
+import { NavLink } from "react-router-dom";
+
+const WorkCard = (props) => {
+  return (
+    <div className="work-container">
+      <h1 className="project-heading">Project</h1>
+      <div className="project-container">
+        <div className="project-card">
+          <img src={props.imgsrc} alt="imgsrc" />
+          <h2 className="project-title">{props.title}</h2>
+          <div className="pro-details">
+            <p>{props.text}</p>
+            <div className="pro-btns">
+              <NavLink to={props.view} className="btn">
+                View
+              </NavLink>
+              <NavLink to={props.source} className="btn">
+                Source
+              </NavLink>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default WorkCard;
